58c98c194353a38b85fec5f16b3a838d9bd5450d
kasdelyne <b2029kaoagnir@pshs.edu.ph>
2025-12-08 13:15:14 +0800

    draft#13

--- public/(3) dashboard.js
+++ public/(3) dashboard.js
@@ -12,6 +12,34 @@
                 }
             }
 
+            function addTask(){
+                const input=document.getElementById("taskInput");
+                const taskText=input.value.trim();
+                
+                if (taskText == " "){
+                    alert("please enter a task !");
+                    return;
+                }
+
+                const li=document.createElement("li");
+
+                const checkbox=document.createElement("input");
+                checkbox.type="checkbox";
+
+                const deleteButton=document.createElement("button");
+                deleteButton.textContent = "X";
+                deleteButton.style.marginLeft = "10px";
+                deleteButton.onclick = () => li.remove();
+
+                li.appendChild(checkbox);
+                li.append(" " + taskText);
+                li.appendChild(deleteButton);
+
+                document.getElementById("todo-list").appendChild(li);
+
+                input.value="";
+            }
+
             /* Spootify*/
             document.getElementById('genreSelect').addEventListener('change', function(){
                 const genre = this.value;
