f0e96700eb2138a1cbae5e1d83804500da912727
kasdelyne <b2029kaoagnir@pshs.edu.ph>
2025-12-08 15:15:03 +0800

    draft#14

--- public/(3) dashboard.js
+++ public/(3) dashboard.js
@@ -8,15 +8,26 @@
             function moreProgress(){
                 if (progress<100){
                     progress+=10;
-                    document.getElementById("prgress-bar").style.width=progress+'%';
+                    document.getElementById("progress-bar").style.width=progress+'%';
                 }
+            }
+
+            /* Add Task*/
+
+            function updateP(){
+                const tasks = document.querySelectorAll("#todo-list input[type='checkbox']");
+                const checked = document.querySelectorAll("#todo-list input[type='checkbox']:checked");
+
+                const percent = tasks.length === 0 ? 0 : (checked.length / tasks.length) * 100;
+
+                document.getElementById("progress-bar").style.width = percent + '%';
             }
 
             function addTask(){
                 const input=document.getElementById("taskInput");
                 const taskText=input.value.trim();
                 
-                if (taskText == " "){
+                if (taskText == ""){
                     alert("please enter a task !");
                     return;
                 }
@@ -25,12 +36,15 @@
 
                 const checkbox=document.createElement("input");
                 checkbox.type="checkbox";
+                checkbox.onchange=updateP;
 
                 const deleteButton=document.createElement("button");
                 deleteButton.textContent = "X";
                 deleteButton.style.marginLeft = "10px";
-                deleteButton.onclick = () => li.remove();
-
+                deleteButton.onclick = () => {
+                    li.remove();
+                    updateP();
+                }
                 li.appendChild(checkbox);
                 li.append(" " + taskText);
                 li.appendChild(deleteButton);
@@ -38,6 +52,7 @@
                 document.getElementById("todo-list").appendChild(li);
 
                 input.value="";
+                updateP();
             }
 
             /* Spootify*/
