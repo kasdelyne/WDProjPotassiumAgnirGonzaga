985a2532b8b3184ead88c58fe10d73b069df0c23
kasdelyne <b2029kaoagnir@pshs.edu.ph>
2025-12-09 21:00:57 +0800

    draft#24

---
+++ public/(4) calendar.js
@@ -0,0 +1,73 @@
+
+            let current = new Date();
+
+            function theCalendar(){
+                let year = current.getFullYear();
+                let month = current.getMonth();
+
+                let monthYear = document.getElementById("monthYear");
+                monthYear.textContent = current.toLocaleString('default',{
+                    month: 'long',
+                    year:'numeric'
+                });
+
+                let daysGrid = document.getElementById("daysGrid");
+                daysGrid.innerHTML = "";
+
+                let firstDay = new Date(year, month, 1).getDay();
+                let lastDate = new Date(year, month +1, 0).getDate();
+
+
+                for(let i=0; i<firstDay; i++){
+                    let emptyBox = document.createElement("div");
+                    daysGrid.appendChild(emptyBox);
+                }
+
+                for(let day=1; day<=lastDate; day++){
+                    let dateKey = year + "-" + (month +1) + "-" + day;
+                    let stored = localStorage.getItem(dateKey);
+                    let event = localStorage.getItem(dateKey);
+                    let dayBox = document.createElement("div");
+                    dayBox.className = "day";
+                    dayBox.textContent = day;
+
+                    if(event){
+                        let save = document.createElement("div");
+                        save.className = "event";
+                        save.textContent = event;
+                        dayBox.appendChild(save);
+                    }
+
+                    dayBox.onclick = function() {
+                        let task = prompt("Add event: ");
+                        if(task){
+                            localStorage.setItem(dateKey, task);
+                            theCalendar();
+                        }
+                    }
+
+                     daysGrid.appendChild(dayBox);  
+                };
+            }
+                function nextMonth(){
+                    current = new Date(current.getFullYear(), current.getMonth() +1, 1);
+                    theCalendar();
+                }
+
+                function prevMonth(){
+                    current = new Date(current.getFullYear(), current.getMonth()-1, 1);
+                    theCalendar();
+                }
+                theCalendar();
+                document.getElementById("next").onclick = nextMonth;
+                document.getElementById("prev").onclick = prevMonth;
+
+            let deleteBtn = document.createElement("button");
+            deleteBtn.textContent = "X";
+            deleteBtn.className = "delete-btn";
+            deleteBtn.onclick = function(e){
+                e.stopPropagation();
+                localStorage.removeItem(dataKey);
+                theCalendar();
+            };
+            save.appendChild(deleteBtn);
